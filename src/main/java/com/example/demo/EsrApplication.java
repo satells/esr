9da4187246ebf979b7f68c637a8618545929728f
unknown <msergiost@hotmail.com>
2023-11-15 20:21:47 -0300

    Arquivos do projeto

---
+++ src/main/java/com/example/demo/EsrApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsrApplication.class, args);
+	}
+
+}
