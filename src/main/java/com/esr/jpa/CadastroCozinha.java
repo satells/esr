df827b2d4d9d04a5ce8444bcc2d23e40cdd3fbed
unknown <msergiost@hotmail.com>
2023-11-21 23:52:08 -0300

    Inserir Cozinha Jpa

--- src/main/java/com/esr/jpa/CadastroCozinha.java
+++ src/main/java/com/esr/jpa/CadastroCozinha.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.esr.domain.model.Cozinha;
 
@@ -18,4 +19,19 @@ public class CadastroCozinha {
 	public List<Cozinha> listar() {
 		return manager.createQuery("from Cozinha c", Cozinha.class).getResultList();
 	}
+
+	@Transactional
+	public Cozinha adicionar(Cozinha cozinha) {
+		/**
+		 * 
+		 * Coloca, incorpora a entidade <Cozinha> dentro do contexto de Persistencia.
+		 * 
+		 */
+
+		// Quando é passado uma instância não há nenhum id atribuido na instância
+		// Cozinha.
+		// Este id é atribuido na instância de retorno do método merge.
+		return manager.merge(cozinha);
+	}
+
 }
--- src/main/java/com/esr/jpa/InclusaoCozinhaMain.java
+++ src/main/java/com/esr/jpa/InclusaoCozinhaMain.java
@@ -1,7 +1,5 @@
 package com.esr.jpa;
 
-import java.util.List;
-
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
@@ -9,17 +7,19 @@ import org.springframework.context.ApplicationContext;
 import com.esr.EsrApplication;
 import com.esr.domain.model.Cozinha;
 
-public class ConsultaCozinhaMain {
+public class InclusaoCozinhaMain {
 
 	public static void main(String[] args) {
 		ApplicationContext context = new SpringApplicationBuilder(EsrApplication.class).web(WebApplicationType.NONE)
 				.run(args);
 
 		CadastroCozinha cadastroCozinha = context.getBean(CadastroCozinha.class);
-		List<Cozinha> cozinhas = cadastroCozinha.listar();
 
-		cozinhas.forEach(cozinha -> System.out.println(cozinha.toString()));
+		Cozinha cozinha = new Cozinha();
+		cozinha.setNome("Minha Cozinha");
+		System.out.println(cozinha);
 
+		Cozinha cozinhaAdicionada = cadastroCozinha.adicionar(cozinha);
+		System.out.println(cozinhaAdicionada);
 	}
-
 }
