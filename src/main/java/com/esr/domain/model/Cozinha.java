e20e5843dec747b935d10329a2a4aacb7da4134b
unknown <msergiost@hotmail.com>
2023-11-22 23:10:12 -0300

    Join Column

--- src/main/java/com/esr/domain/model/Cozinha.java
+++ src/main/java/com/esr/domain/model/Cozinha.java
@@ -20,7 +20,7 @@ public class Cozinha {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(unique = true)
+	@Column(unique = true, nullable = false)
 	private String nome;
 
 }
